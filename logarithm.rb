71fcaeb8532a81ecac16a96dc22b7d58f34c110b
harazono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2018-12-28 22:13:09 +0900

    WIP

--- logarithm.rb
+++ logarithm.rb
@@ -12,13 +12,14 @@ if ARGV.size < 1
 end
 
 table = CSV.table(ARGV[0], headers:false)
-alt = table.map{ |row| row.map{|col| log10(col) + 3} }
+alt = table.map{ |row| row.map{|col| (log10(col) + 2.5) * 100} }
 for i in 0..alt.size-1
   for j in 0..alt.size-1
     if alt[j][i].finite? then
-      printf("%2d", alt[j][i].round)
+      printf("%4d", alt[j][i].round)
     else
-      printf("%2d", -5)
+#      printf("-inf")
+      printf("%11d", -1 * (2 ** 30))
     end
     printf(", ") if j != alt.size-1
   end
