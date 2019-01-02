1c6f921cddacb3923045dde569e232a2a5a41bf2
harazono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2019-01-02 18:44:30 +0900

    WIP

--- scorerize.rb
+++ scorerize.rb
@@ -4,39 +4,40 @@ require 'matrix'
 include Math
 
 def usage
-  print "logarithm.rb <table.csv>\n"
+  print "scorerize.rb <table.csv>\n"
 end
 if ARGV.size < 1
   usage
   exit
 end
-
 table = CSV.table(ARGV[0], headers:false)
-alt = table.map{ |row| row.map{|col| (log10(col) + 1.5) * 100} }
-=begin
-puts <<"EOS"
-#ifndef _SCORE_2MER_MOD_3DIG
-#define _SCORE_2MER_MOD_3DIG
-int score_2mer_mod_3digit[] = {
-EOS
-=end
+k = 1
+for i in 1..6 do
+  if 5 ** i == table[0].size
+    k = i
+  end
+end
+diff = -30 if k == 1
+diff = 200 if k == 2
+diff = 250 if k == 3
+
+alt = table.map{ |row| row.map{|col| log10(col) * 100} }
+head_string = "#ifndef _SCORE_#{k}MER_MOD_3DIG\n#define _SCORE_#{k}MER_MOD_3DIG\nint score_#{k}mer_mod_3digit[] = {\n"
+puts head_string
 for i in 0..alt.size-1
   for j in 0..alt.size-1
     if alt[j][i].finite? then
-      printf("%4d", alt[j][i].round)
+      printf("%5d", alt[j][i].round + diff)
     else
-#      printf("%d", -1 * (2 ** 30))
-#      printf("%11d", -1 * (2 ** 30))
-       printf("-inf")
+      printf("%5d", -1 * (2 ** 10) + diff)
     end
     printf(", ") if j != alt.size-1
   end
 #  print "\n"
-  if i != alt.size-1 then print "\n" else print "\n" end
+  if i != alt.size-1 then print ",\n" else print "\n" end
 end
-=begin
+
 puts <<"EOS"
 };
 #endif
 EOS
-=end
