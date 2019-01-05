b221705f99c1e11d8fd42bd6a0ab20ad3bc9d78e
harazono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2019-01-05 19:33:17 +0900

    WIp

--- prob2scoremtx.sh
+++ prob2scoremtx.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-for i in 1 2 3 4 5 6 ; do
-  ./scorerize.rb ../Ecoli/2452_${i}mer_mod.csv > ./kenpin_${i}.h
+for i in 1 2 3 4 5 ; do
+  ./scorerize.rb ../Ecoli/2452_${i}mer_mod.csv > ../minimi/src/score_${i}mer_mod_3digit.h
 done
 
