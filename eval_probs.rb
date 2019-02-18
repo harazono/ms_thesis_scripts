13a8400cdc750f2a779ca19d26a9f963765e9ad5
harazono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2019-02-18 13:15:45 +0900

    WIP

--- eval_probs.rb
+++ eval_probs.rb
@@ -18,16 +18,12 @@ def print_probs(probs, tablesize)
   index = tablesize - 1
   for i in [*0..index] do
     for j in [*0..index] do
-      print "#{probs[i][j]}"
+      print sprintf("%10f", probs[j][i].to_f)
       print ", " if j != index
     end
     puts
   end
 end
-
-
-
-
 
 def usage
   print "eval_probs.rb <1mer_ocurrence.csv> <2mer_ocurrence.csv>\n"
@@ -48,5 +44,23 @@ probs2 = Array.new(25).map{Array.new(25, 0)}
 
 count_probs(probs1, table1, 5)
 count_probs(probs2, table2, 25)
-print_probs(probs2, 25)
 
+
+probs2byProbs1 = Array.new(25).map{Array.new(25, 0)}
+
+for i in [*0..24] do
+  for j in [*0..24] do
+    refidx_head = i / 5
+    refidx_tail = i % 5
+    qryidx_head = j / 5
+    qryidx_tail = j % 5
+    probs2byProbs1[i][j] = probs1[refidx_head][qryidx_head].to_f * probs1[refidx_tail][qryidx_tail].to_f
+  end
+end
+
+
+print_probs(probs1, 5)
+puts
+print_probs(probs2byProbs1, 25)
+puts
+print_probs(probs2, 25)
